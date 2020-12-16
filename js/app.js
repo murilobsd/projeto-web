9fc3c00d91a04922a8707eb0addf4d6268acd9a9
mbsd <mbsd@m0x.ru>
2020-12-15 22:03:05 -0300

    refact modal

---
+++ js/app.js
@@ -0,0 +1,11 @@
+// Get the modal
+var modal_cond = document.getElementById('id01');
+var modal_pol = document.getElementById('id02');
+var modal_fis = document.getElementById('id03');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
